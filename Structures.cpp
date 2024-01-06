18e5a58728b9b370a65295894eef1128c21c7f7d
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-01-06 10:56:33 +0530

    Create structures.cpp

---
+++ Structures.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Structures in cpp
+    struct {
+        int a=10;
+        string s="Shilpa";
+    }myfirst;
+    cout<<myfirst.a<<endl;
+    cout<<myfirst.s;
+}
