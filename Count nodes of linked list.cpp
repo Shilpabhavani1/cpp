18bf04d47bc74dc5782fa4e9320ff39e51a6675c
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2024-01-09 18:50:13 +0530

    Create Count nodes of linked list.cpp

---
+++ Count nodes of linked list.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+    
+        //Code here
+        Node* t=head;
+        while(t!=NULL)
+        {
+            t=t->next;
+        }
+        return head;
+    
+    }
+};
+    
